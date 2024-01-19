57e38d0b6cb90c14c7bdc12e131787a34df9573d
bilsina <bils1n@icloud.com>
2024-01-19 16:08:48 +0600

    icacoe v0.01

--- src/main/java/com/example/tictactoe/HelloController.java
+++ src/main/java/com/example/tictactoe/HelloController.java
@@ -4,12 +4,30 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Line;
 
 public class HelloController {
 
     @FXML
     private String currentSymbol = "X";
     private String[][] gameField = new String[3][3];
+
+    @FXML
+    private Line winLine1;
+    @FXML
+    private Line winLine2;
+    @FXML
+    private Line winLine3;
+    @FXML
+    private Line winLine4;
+    @FXML
+    private Line winLine5;
+    @FXML
+    private Line winLine6;
+    @FXML
+    private Line winLine7;
+    @FXML
+    private Line winLine8;
 
     @FXML
     void btnClick(ActionEvent event) {
@@ -19,9 +37,67 @@ public class HelloController {
         int columnIndex = GridPane.getColumnIndex(button) == null ? 0 : GridPane.getColumnIndex(button);
         if (gameField[rowIndex][columnIndex] == null) {
             gameField[rowIndex][columnIndex] = currentSymbol;
-            currentSymbol = currentSymbol.equals("X") ? "O" : "X";
             button.setText(currentSymbol);
+            currentSymbol = currentSymbol.equals("X") ? "O" : "X";
+            checkFinish(rowIndex, columnIndex);
         }
+    }
+
+    private void checkFinish(int row, int column) {
+        if (this.gameField[row] != null) {
+            if (checkRow(row)) {
+                if (winLine5 != null) winLine5.setOpacity(1.0);
+            } else if (checkColumn(column)) {
+                if (winLine4 != null) winLine4.setOpacity(1.0);
+            } else if (checkDiagonalRight() || checkDiagonalLeft()) {
+                if (winLine2 != null) winLine2.setOpacity(1.0);
+            }
+        }
+    }
+
+    private boolean checkRow(int row) {
+        boolean isWin = true;
+        for (int i = 0; i < gameField[row].length; i++) {
+            if (gameField[row][i] == null || gameField[row][i].equals("O")) {
+                isWin = false;
+                return isWin;
+            }
+        }
+        return isWin;
+    }
+
+    private boolean checkColumn(int column) {
+        boolean isWin = true;
+        for (int i = 0; i < gameField.length; i++) {
+            if (gameField[i][column] == null || gameField[i][column].equals("O")) {
+                isWin = false;
+                return isWin;
+            }
+        }
+        return isWin;
+    }
+
+    private boolean checkDiagonalLeft() {
+        boolean isWin = true;
+        for (int i = 0; i < gameField.length; i++) {
+            if (gameField[i][i] == null || gameField[i][i].equals("O")) {
+                isWin = false;
+                return isWin;
+            }
+        }
+        return isWin;
+    }
+
+    private boolean checkDiagonalRight() {
+        boolean isWin = true;
+        for (int i = 0; i < gameField.length; i++) {
+            if (gameField[i][Math.abs(i - 2)] == null || gameField[i][Math.abs(i - 2)].equals("O")) {
+                System.out.println("ok");
+                isWin = false;
+                return isWin;
+            }
+        }
+        return isWin;
     }
 
 }
